b802c7fc04cc737191009ed814eea5fd93eeb692
Sanika Suryawanshi <sanikasuryawanshi0305@gmail.com>
2022-08-26 15:18:01 +0000

    Added questions for the quiz

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 var readlineSync = require('readline-sync');
+
+var questions = [{
+    question : "Who sung the song 'Go Flex' ? ",
+    answer : 'Post Malone'
+  },{
+    question : "Who sung the song 'This Town' ? ",
+    answer : 'Niall Horan'
+  },{
+    question : "Who wrote the song 'Drivers License' ? ",
+    answer : 'Olivia Rodrigo'
+  },{
+    question : "The song 'Drivers License' is from which album ? ",
+    answer : 'Sour'
+  },{
+    question : "One Direction's song 'Night Changes' is from which album ? ",
+    answer : 'Four'
+  }];
